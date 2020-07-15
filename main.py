2c32085e520a42f537f1796e01e39dcf358ea5e7
Peter Phillips <p.phillips59311@gmail.com>
2020-07-15 16:38:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from socket import gethostname
+
 
 app = Flask(__name__)
 
@@ -7,4 +9,5 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if 'liveconsole' not in gethostname():
+        app.run(debug=True)
